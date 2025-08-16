81df9fd06a96627538f427d2efb8f4fcda173c0c
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-08-16 14:33:21 +0530

    Update artist-song.model.ts

--- src/modules/artists/models/artist-song.model.ts
+++ src/modules/artists/models/artist-song.model.ts
@@ -1,13 +1,23 @@
-import { SimpleSongModel } from '#modules/songs/models'
+import { SongAPIResponseModel, SongModel } from '#modules/songs/models'
+import { z } from 'zod'
 
-// Suppose rawSongs = ArtistAPIResponseModel se aaya topSongs array
-const simplifiedSongs = rawSongs.topSongs.map(song => ({
-  id: song.id,
-  title: song.title
-}))
+export const ArtistSongAPIResponseModel = z.object({
+  artistId: z.string(),
+  name: z.string(),
+  subtitle: z.string(),
+  image: z.string(),
+  follower_count: z.string(),
+  type: z.string(),
+  isVerified: z.boolean(),
+  dominantLanguage: z.string(),
+  dominantType: z.string(),
+  topSongs: z.object({
+    songs: z.array(SongAPIResponseModel),
+    total: z.number()
+  })
+})
 
-// Return JSON
-return {
-  total: simplifiedSongs.length,
-  songs: simplifiedSongs
-}
+export const ArtistSongModel = z.object({
+  total: z.number(),
+  songs: z.array(SongModel)
+})
